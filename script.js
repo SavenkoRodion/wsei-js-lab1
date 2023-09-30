6f8eb6fd226133711bcf4f30d79d3c0d75405f3c
Rodion Savenko <rodion.savenko@microsoft.wsei.edu.pl>
2023-09-30 16:08:20 +0200

    shame refactored

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 console.log("hello")
-
 const mainButton = document.querySelector("#mainButton")
-
-const validationRule = (inputValue) => isNaN(inputValue) || inputValue === ""
 const minOutput = document.querySelector("#min")
 const maxOutput = document.querySelector("#max")
 const avgOutput = document.querySelector("#avg")
 const errorOutput = document.querySelector("#error");
 
-const input1 = document.querySelector("#liczba1").value;
-const input2 = document.querySelector("#liczba2").value;
-const input3 = document.querySelector("#liczba3").value;
-const input4 = document.querySelector("#liczba4").value;
+let input1 = document.querySelector("#liczba1");
+let input2 = document.querySelector("#liczba2");
+let input3 = document.querySelector("#liczba3");
+let input4 = document.querySelector("#liczba4");
 
-mainButton.addEventListener("click", () => {
-    
-    
+const validationRule = (inputValue) => isNaN(inputValue) || inputValue === "";
 
-    if(validationRule(input1) || validationRule(input2) || validationRule(input3) || validationRule(input4))
+const calculations = () => 
+{
+    let number1 = input1.value;
+    let number2 = input2.value;
+    let number3 = input3.value;
+    let number4 = input4.value;
+
+    if(validationRule(number1) || validationRule(number2) || validationRule(number3) || validationRule(number4))
     {
         minOutput.innerHTML = "";
         maxOutput.innerHTML = "";
@@ -25,16 +27,19 @@ mainButton.addEventListener("click", () => {
 
         errorOutput.innerHTML = "Wypełnij poprawnie wszystkie pola"
 
-        return
+        return;
     }
 
     errorOutput.innerHTML = ""
 
-    input1 = Number(input1);
-    input2 = Number(input2);
-    input3 = Number(input3);
-    input4 = Number(input4);
-    minOutput.innerHTML = Math.min(input1, input2, input3, input4);
-    maxOutput.innerHTML = Math.max(input1, input2, input3, input4);
-    avgOutput.innerHTML = (input1 + input2 + input3 + input4) / 4;  
-})
+    number1 = Number(number1);
+    number2 = Number(number2);
+    number3 = Number(number3);
+    number4 = Number(number4);
+    minOutput.innerHTML = Math.min(number1, number2, number3, number4);
+    maxOutput.innerHTML = Math.max(number1, number2, number3, number4);
+    avgOutput.innerHTML = (number1 + number2 + number3 + number4) / 4;
+}
+
+mainButton.addEventListener("click", () => calculations())
+
