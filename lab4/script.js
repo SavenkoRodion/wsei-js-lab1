db798a5015bd4901b9794fcb9d3ee62ae358bcbf
rodion.savenko <rodion.savenko@microsoft.wsei.edu.pl>
2024-01-27 16:11:07 +0100

    Lab4 in progress

--- lab4/script.js
+++ lab4/script.js
@@ -2,35 +2,64 @@ console.log("Hello world");
 
 const appWrapper = document.querySelector("#app-wrapper");
 
+class Card {
+    constructor(id){
+        this.id = id;
+        this.header;
+        this.body;
+        this.isSaved = false;
+    }
+}
+
+const cardsArray = [];
+
+
+
+const saveCardData = (id) => {
+    console.log("Card with id "+id+" is saved")
+    cardsArray[id]; //Create method
+}
+
 const createCard = ()=>{
+    const lol = new Card(cardsArray.length)
+
     const cardWrapper = document.createElement("div");
-    const cardHeader = document.createElement("div");
-    const cardBody = document.createElement("div");
-    const cardFooter = document.createElement("div");
+    const cardHeaderWrapper = document.createElement("div");
+    const cardBodyWrapper = document.createElement("div");
+    const cardFooterWrapper = document.createElement("div");
     const cardHeaderInput = document.createElement("input");
     const cardBodyTextarea = document.createElement("textarea");
     const cardFooterSaveBtn = document.createElement("button");
     const cardFooterRemoveBtn = document.createElement("button");
     
     cardWrapper.classList.add("card");
-    cardHeader.classList.add("card-header");
-    cardBody.classList.add("card-body");
-    cardFooter.classList.add("card-footer");
+    cardWrapper.id="";
+    cardHeaderWrapper.classList.add("card-header");
+    cardBodyWrapper.classList.add("card-body");
+    cardFooterWrapper.classList.add("card-footer");
     
     cardFooterSaveBtn.innerHTML="Save";
+    cardFooterSaveBtn.addEventListener("click", () => { saveCardData(lol.id) });
     cardFooterRemoveBtn.innerHTML="Remove";
 
-    cardHeader.appendChild(cardHeaderInput);
-    cardBody.appendChild(cardBodyTextarea);
-    cardFooter.appendChild(cardFooterSaveBtn);
-    cardFooter.appendChild(cardFooterRemoveBtn);
+    cardHeaderInput.required = true;
+    cardBodyTextarea.required = true;
+
+    cardHeaderWrapper.appendChild(cardHeaderInput);
+    cardBodyWrapper.appendChild(cardBodyTextarea);
+    cardFooterWrapper.appendChild(cardFooterSaveBtn);
+    cardFooterWrapper.appendChild(cardFooterRemoveBtn);
 
 
-    cardWrapper.appendChild(cardHeader, cardBody, cardFooter);
-    cardWrapper.appendChild(cardBody);
-    cardWrapper.appendChild(cardFooter);
+    cardWrapper.appendChild(cardHeaderWrapper, cardBodyWrapper, cardFooterWrapper);
+    cardWrapper.appendChild(cardBodyWrapper);
+    cardWrapper.appendChild(cardFooterWrapper);
     
+    cardsArray.push(lol)
     appWrapper.appendChild(cardWrapper);
+
+    console.log(cardsArray)
+
     return true;
 }
 
